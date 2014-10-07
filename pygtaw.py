be048b5c57961ba2ffa080ba5dcf698ab9d0341b
Steph Samson <sdvsamson@gmail.com>
2014-10-07 17:13:12 -0400

    Unicode support for pygtaw.py

--- pygtaw.py
+++ pygtaw.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import requests
 from langs import langs
 
@@ -8,14 +9,15 @@ class Client(object):
         self._response = None
         self._source = None
 
-    def build_translation_request(self, query, source, target):
+    def build_translation_request(self, query, target, source):
         payload = dict(q=query, target=target, **self._payload)
         if source:
             payload['source'] = source
         return payload
 
-    def translate(self, query, source=None, target='Spanish'):
-        payload = self.build_translation_request(query, langs.get(source, None), langs[target])
+    def translate(self, query, target='Spanish', source=None):
+        query.decode(encoding='utf-8')
+        payload = self.build_translation_request(query, langs[target], langs.get(source, None))
         r = requests.get(self._url, params=payload)
         self._source = source
         self._response = r.json()['data']['translations'][0]
