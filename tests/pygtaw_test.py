6d131b32739177d8c2f6d5eda72833166cf75b33
Steph Samson <sdvsamson@gmail.com>
2014-10-09 12:03:37 -0400

    More tests for basic functionality.

--- tests/pygtaw_test.py
+++ tests/pygtaw_test.py
@@ -12,8 +12,21 @@ class TestPyGoogleTranslateAPIWrapper(unittest.TestCase):
         self.client = pygtaw.Client(api_key)
 
     def test_get_translated_text(self):
-        hola = self.client.translate('Hola', 'English')
-        self.assertEqual(hola.translated_text, 'Hi')
+        query = self.client.translate('Hola', 'English')
+        self.assertEqual(query.translated_text, 'Hi')
+
+    def test_get_detected_source_lang(self):
+        query = self.client.translate('Hello', 'German')
+        self.assertEqual(query.detected_source_language, 'English')
+
+    def test_get_translated_text_non_standard_ascii(self):
+        query = self.client.translate('World', 'Korean')
+        self.assertEqual(query.translated_text, u'\uc138\uacc4')
+
+    def test_try_get_detected_source_lang(self):
+        query = self.client.translate('El mundo', target='English', source='Spanish')
+        self.assertEqual(query.detected_source_language,
+            'No detected source language, source provided by user: Spanish')
 
     def tearDown(self):
         self.make_client()
