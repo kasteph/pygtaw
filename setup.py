360d6580f7221c110b38ac3d8bfd82526cfa416b
Steph Samson <sdvsamson@gmail.com>
2014-10-21 10:17:29 -0400

    Ver up

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pygtaw",
-    version="0.0.7",
+    version="0.0.8",
     author="Steph Samson",
     author_email="sdvsamson@gmail.com",
     description=("A tiny Google Translate API wrapper written in Python."),
